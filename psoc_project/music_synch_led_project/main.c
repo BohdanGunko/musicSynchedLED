59a65e7075e75498b75bd93a69039064b1814a98
Bohdan Hunko <77784135+hunkob@users.noreply.github.com>
2022-04-21 20:18:28 +0300

    WS2812 control library added (#2)

--- psoc_project/music_synch_led_project/main.c
+++ psoc_project/music_synch_led_project/main.c
@@ -1,141 +1,44 @@
-/******************************************************************************
-* File Name:   main.c
-*
-* Description: This is the source code for the PSoC 6 MCU Secure Blinky FreeRTOS
-*              Example for ModusToolbox.
-*
-* Related Document: See Readme.md
-*
-*
-*******************************************************************************
-* Copyright 2020-2021, Cypress Semiconductor Corporation (an Infineon company) or
-* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
-*
-* This software, including source code, documentation and related
-* materials ("Software") is owned by Cypress Semiconductor Corporation
-* or one of its affiliates ("Cypress") and is protected by and subject to
-* worldwide patent protection (United States and foreign),
-* United States copyright laws and international treaty provisions.
-* Therefore, you may use this Software only as provided in the license
-* agreement accompanying the software package from which you
-* obtained this Software ("EULA").
-* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
-* non-transferable license to copy, modify, and compile the Software
-* source code solely for use in connection with Cypress's
-* integrated circuit products.  Any reproduction, modification, translation,
-* compilation, or representation of this Software except as specified
-* above is prohibited without the express written permission of Cypress.
-*
-* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
-* reserves the right to make changes to the Software without notice. Cypress
-* does not assume any liability arising out of the application or use of the
-* Software or any product or circuit described in the Software. Cypress does
-* not authorize its products for use in any products where a malfunction or
-* failure of the Cypress product may reasonably be expected to result in
-* significant property damage, injury or death ("High Risk Product"). By
-* including Cypress's product in a High Risk Product, the manufacturer
-* of such system or application assumes all risk of such use and in doing
-* so agrees to indemnify Cypress against all liability.
-*******************************************************************************/
-
 #include "cy_pdl.h"
 #include "cyhal.h"
 #include "cybsp.h"
 #include "cy_retarget_io.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "ws2812_lib/ws2812.h"
+#include "helper_utils.h"
 
+/* Defines for blinky LEDs task */
+#define BLINKY_LEDS_TASK_NAME       ("Blinky LEDs task")
+#define BLINKY_LEDS_TASK_STACK_SIZE (2 *1024)
+#define BLINKY_LEDS_TASK_PRIORITY   (5)
 
-/*******************************************************************************
-* Macros
-********************************************************************************/
+void blinky_leds_task(void *arg);
 
-/* Defines for Blinky LED task */
-#define BLINKY_LED_TASK_NAME         ("Blinky Task")
-#define BLINKY_LED_TASK_STACK_SIZE   (1024)
-#define BLINKY_LED_TASK_PRIORITY     (5)
-#define BLINKY_LED_TASK_DELAY_TICKS  (500)
-
-/* Defines for Counter task */
-#define COUNTER_TASK_NAME                ("Counter Task")
-#define COUNTER_TASK_STACK_SIZE          (1024)
-#define COUNTER_TASK_PRIORITY            (5)
-#define COUNTER_LED_TASK_DELAY_TICKS     (1000)
-
-UBaseType_t __attribute__((used)) uxTopUsedPriority;
-
-#define uxTopReadyPriority uxTopUsedPriority
-
-/*******************************************************************************
-* Function Prototypes
-********************************************************************************/
-void blinky_task( void * arg );
-void counter_task( void * arg );
-
-/*******************************************************************************
-* Global Variables
-********************************************************************************/
-
-
-/*******************************************************************************
-* Function Name: main
-********************************************************************************
-* Summary:
-* This is the main function for CM4 CPU. It does...
-*    1. Creates FreeRTOS tasks
-*    2. Starts FreeRTOS scheduler
-*
-* Parameters:
-*  void
-*
-* Return:
-*  int
-*
-*******************************************************************************/
 int main(void)
 {
-    cy_rslt_t result;
-    int retval;
-
-    uxTopUsedPriority = configMAX_PRIORITIES - 1;
+    cy_rslt_t cy_res;
+    BaseType_t rtos_res;
 
     /* Initialize the device and board peripherals */
-    result = cybsp_init() ;
-    if (result != CY_RSLT_SUCCESS)
-    {
-        CY_ASSERT(0);
-    }
+    cy_res = cybsp_init();
+    CY_ASSERT(CY_RSLT_SUCCESS == cy_res);
 
     /* Enable global interrupts */
-         __enable_irq();
+    __enable_irq();
 
     /* Initialize retarget-io to use the debug UART port */
     cy_retarget_io_init(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX, CY_RETARGET_IO_BAUDRATE);
 
-    printf("\r\n***** PSoC 64 MCU: Secure Blinky LED FreeRTOS Example ***** \r\n\n");
+    /* Clear screen */
+    printf("\033[2J");
+    printf("\r\n***** LED control app started *****\r\n\n");
     printf("Build date %s and time %s\r\n\n", __DATE__, __TIME__);
 
     /* Create FreeRTOS task */
-    retval = xTaskCreate( blinky_task, BLINKY_LED_TASK_NAME,BLINKY_LED_TASK_STACK_SIZE, NULL, BLINKY_LED_TASK_PRIORITY, NULL );
+    rtos_res = xTaskCreate(blinky_leds_task, BLINKY_LEDS_TASK_NAME, BLINKY_LEDS_TASK_STACK_SIZE, NULL, BLINKY_LEDS_TASK_PRIORITY, NULL);
+    ASSERT_WITH_PRINT(pdPASS == rtos_res, "%s didn't started!\r\n", BLINKY_LEDS_TASK_NAME);
 
-    if( pdPASS != retval )
-    {
-         printf("%s did not start!\r\n", BLINKY_LED_TASK_NAME);
-    }
-
-    /* Create FreeRTOS task */
-    retval = xTaskCreate( counter_task, COUNTER_TASK_NAME, COUNTER_TASK_STACK_SIZE, NULL, COUNTER_TASK_PRIORITY, NULL );
-
-    if( pdPASS == retval )
-    {
-        vTaskStartScheduler();
-    }
-    else
-    {
-        printf("%s did not start!\r\n", COUNTER_TASK_NAME);
-    }
+    vTaskStartScheduler();
 
     for(;;)
     {
@@ -143,69 +46,27 @@ int main(void)
     }
 }
 
-/*******************************************************************************
-* Function Name: blinky_task
-********************************************************************************
-* Summary:
-* This is the FreeRTOS task callback function. It does...
-*    1. Toggles LED for every 500ms in a tight for loop
-*
-* Parameters:
-*  void * context passed from main function
-*
-* Return:
-*  void
-*
-*******************************************************************************/
-void blinky_task( void * arg )
+void blinky_leds_task(void *arg)
 {
     (void)arg;
+    ws2818_res_t ws_res;
 
-    printf("%s started!\r\n", BLINKY_LED_TASK_NAME);
+    printf("%s started!\r\n", BLINKY_LEDS_TASK_NAME);
 
-    /* Initialize the User LED */
-    cyhal_gpio_init((cyhal_gpio_t) CYBSP_USER_LED1, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_STRONG, CYBSP_LED_STATE_OFF);
+    /* MISO and SCLK are not needed sothey are not connected (NC) */
+    ws_res = ws2812_init(CYBSP_A0, NC, NC);
+    ASSERT_WITH_PRINT(ws2812_success == ws_res, "ws2812_init failed\r\n");
 
     for(;;)
     {
-        /* Toggle the user LED periodically */
-        vTaskDelay(BLINKY_LED_TASK_DELAY_TICKS ) ;
-
-        /* Invert the USER LED state */
-        cyhal_gpio_toggle((cyhal_gpio_t) CYBSP_USER_LED1);
+        ws2812_set_all_leds(255, 0, 0);    // Set all LEDs to RED at max brighness
+        ws2812_update();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        ws2812_set_all_leds(0, 255, 0);    // Set all LEDs to GREEN at max brighness
+        ws2812_update();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        ws2812_set_all_leds(0, 0, 255);    // Set all LEDs to BLUE at max brighness
+        ws2812_update();
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
-
-/*******************************************************************************
-* Function Name: counter_task
-********************************************************************************
-* Summary:
-* This is the FreeRTOS task callback function. It does...
-*    1. Increments a counter and prints to terminal
-*
-* Parameters:
-*  void * context passed from main function
-*
-* Return:
-*  void
-*
-*******************************************************************************/
-void counter_task( void * arg )
-{
-    (void)arg;
-    uint16 count = 0;
-
-    printf("%s started!\r\n", COUNTER_TASK_NAME);
-
-    for(;;)
-    {
-        /* Delay the print counter */
-        vTaskDelay(COUNTER_LED_TASK_DELAY_TICKS ) ;
-
-        /* Print out the counter number */
-        printf("%d\r\n", count++);
-    }
-}
-
-
-/* [] END OF FILE */
