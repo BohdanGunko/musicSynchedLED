6cd5eee1b2fdd9f665f70370d5f10d7d086f583b
Bohdan Hunko <77784135+hunkob@users.noreply.github.com>
2022-04-28 23:54:33 +0300

    ADC sampling works (#6)

--- psoc_project/music_synch_led_project/app_config.h
+++ psoc_project/music_synch_led_project/app_config.h
@@ -4,8 +4,20 @@
 /* Number of LEDs */
 #define WS2812_LEDS_COUNT   (30)
 
-/* FFT related definitions */
+/* Pin to which ws2812 data line is connected */
+#define WS2812_LEDS_PIN     (CYBSP_A0)
+
+/* FFT_SIZE Must be a power of 2 in range from 16 to 4096 */
 #define FFT_SIZE            (1024)
 #define FFT_SIZE_HALF       (FFT_SIZE / 2)
+
+/* Pin to sample audio signal from */
+#define AUDIO_SAMPLING_PIN  (CYBSP_A1)
+
+/* Sample rate for ADC in Hz */
+#define AUDIO_SAMPLING_RATE (44100)
+
+/* Whether to measure performance */
+#define MEASURE_PERFORMANCE (1)
 
 #endif /* __APP_CONFIG_H__ */
